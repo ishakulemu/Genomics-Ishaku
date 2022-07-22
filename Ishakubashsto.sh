1a3d3d15eddf35962519dff72e6a5834a8561388
ishakulemu <109811728+ishakulemu@users.noreply.github.com>
2022-07-22 19:33:18 +0100

    Add files via upload

---
+++ Ishakubashsto.sh
@@ -0,0 +1,52 @@
+# Bash story 1
+
+    username1="Ishak"
+    username2="Harun"
+    echo username1 
+    echo username2
+    mkdir Ishak
+    mkdir bayocomputin && cd bayocomputin
+    wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+    wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+    mv wildtype.fna ../Ishak/
+    ls
+    rm wildtype.gbk.1
+    ls
+    cd ../Ishak/
+    ls
+    grep 'tatatata' wildtype.fna
+    grep 'tatatata' wildtype.fna > mutant
+    clear
+    ls && ls ../bayocomputin/
+    exit ()
+
+
+# Bash story 2
+
+
+    sudo apt-get install figlet
+    figlet Ishak
+    mkdir compaya && cd compaya
+    wget https://www.bioinformatics.babraham.ac.uk/training/Introduction%20to%20Unix/unix_intro_data.tar.gz
+    ls
+    gunzip unix_intro_data.tar.gz
+    ls
+    tar xfv unix_intro_data.tar
+    ls
+    cd seqmonk_genomes
+    ls
+    cd 'Saccharomyces cerevisiae'
+    ls
+    cd EF4
+    ls
+    cp Mito.dat ../../../compaya
+    ls
+    nano Mito.dat
+    nano Mitochondrion.txt
+    cd ../
+    cd ../
+    cd ../
+    ls
+    cd FastQ_Data
+    ls
+    gunzip -c lane8_DD_P4_TTAGGC_L008_R1.fastq.gz | wc -l
